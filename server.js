0087ef2c9ce8d5c15ca0814be920695c3a6cd53a
Lars Kruse <lakruzz@inc-inc.dk>
2022-12-20 19:16:37 +0100

    Spelling

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.use('/', require('./routes/root'))
 app.listen(PORT,() => console.log(`Server running on port ${PORT}`))  
 
 app.all('*', (req, res) => {
-    res.status(404)
+    res.status(404)   
     if (req.accepts('html')){
-        res.sendFile(path.join(__dirname), 'views', '404.html')
+          res.sendFile(path.join(__dirname, 'views', '404.html'))
     } else if ( req.accepts('json')) {
         res.json( { message: '404 Not Found'}) 
     } else {
